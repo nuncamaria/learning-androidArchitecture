8bd2f44279a190def5f174fa60269371c492017d
nuncamaria <nunezcanovas@gmail.com>
2024-12-06 21:56:46 +0100

    add error string messages

--- app/src/main/java/com/nuncamaria/learningandroidarchitecture/core/domain/util/NetworkError.kt
+++ app/src/main/java/com/nuncamaria/learningandroidarchitecture/core/domain/util/NetworkError.kt
@@ -1,5 +1,7 @@
 package com.nuncamaria.learningandroidarchitecture.core.domain.util
 
+import com.nuncamaria.learningandroidarchitecture.R
+
 enum class NetworkError : Error {
     REQUEST_TIMEOUT,
     TOO_MANY_REQUESTS,
@@ -7,4 +9,17 @@ enum class NetworkError : Error {
     SERVER_ERROR,
     SERIALIZATION,
     UNKNOWN
+}
+
+fun NetworkError.getStringRes(): Int {
+    val resId = when (this) {
+        NetworkError.REQUEST_TIMEOUT -> R.string.request_timeout
+        NetworkError.TOO_MANY_REQUESTS -> R.string.too_many_requests
+        NetworkError.NO_INTERNET_CONNECTION -> R.string.no_internet_connection
+        NetworkError.SERVER_ERROR -> R.string.server_error
+        NetworkError.SERIALIZATION -> R.string.serialization
+        NetworkError.UNKNOWN -> R.string.unknown
+    }
+
+    return resId
 }
