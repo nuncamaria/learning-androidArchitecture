5537c3688cbdadf56b7c6d9d1c3d35fae11a32b4
nuncamaria <nunezcanovas@gmail.com>
2024-12-06 12:47:19 +0100

    setup: replace paths

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 
 android {
-    namespace = "com.plcoding.cryptotracker"
+    namespace = "com.nuncamaria.learningandroidarchitecture"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.plcoding.cryptotracker"
+        applicationId = "com.nuncamaria.learningandroidarchitecture"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
