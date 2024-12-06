76b6815ba71240501c27e45f632b81574cb8bfff
nuncamaria <nunezcanovas@gmail.com>
2024-12-06 17:54:24 +0100

    add spacing enum for ui

---
+++ app/src/main/java/com/nuncamaria/learningandroidarchitecture/ui/theme/Spacing.kt
@@ -0,0 +1,23 @@
+package com.nuncamaria.learningandroidarchitecture.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class Spacing(val dp: Dp) {
+    NONE(0.dp),
+    XXXS(2.dp),
+    XXS(4.dp),
+    XS(8.dp),
+    S(12.dp),
+    MD(16.dp),
+    LG(24.dp),
+    XL(32.dp),
+    XXL(40.dp),
+    XXXL(44.dp),
+    JUMBO(48.dp),
+    XJUMBO(56.dp),
+    XXJUMBO(64.dp),
+    XXXJUMBO(72.dp),
+    GIGA(80.dp),
+    MEGA(128.dp)
+}
